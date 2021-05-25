63ac41d49d4c47b9d3bbd3467a5343dd40a3f943
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-05-25 23:26:32 +0300

    Add test and function for searching by number.

--- contact.py
+++ contact.py
@@ -27,3 +27,16 @@ class Contact:
         delete_contact method deletes a saved contact from the contact_list
         '''
         Contact.contact_list.remove(self)
+    @classmethod
+    def find_by_number(cls,number):
+        '''
+        Method that takes in a number and returns a contact that matches that number.
+
+        Args:
+            number: Phone number to search for
+        Returns :
+            Contact of person that matches the number.
+        '''
+        for contact in cls.contact_list:
+            if contact.phone_number == number:
+                return contact
--- contact_test.py
+++ contact_test.py
@@ -50,6 +50,15 @@ class TestContact(unittest.TestCase):
         test_contact.save_contact()
         self.new_contact.delete_contact()
         self.assertEqual(len(Contact.contact_list),1)
+    def test_find_contact_by_number(self):
+        '''
+        test to check if we can find a contact by phone number and display information
+        '''
+        self.new_contact.save_contact()
+        test_contact = Contact("Test","user","0711223344","test@user.com")
+        test_contact.save_contact()
+        found_contact = Contact.find_by_number("0711223344")
+        self.assertEqual(found_contact.email,test_contact.email)
 
 if __name__ == '__main__':
     unittest.main()
