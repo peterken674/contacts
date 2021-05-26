e20affb22a534b110e3aa0180b1dfbf066f7eb1e
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-05-26 08:11:43 +0300

    Add function to check whether a contact exists in the contact list.

--- contact.py
+++ contact.py
@@ -40,3 +40,16 @@ class Contact:
         for contact in cls.contact_list:
             if contact.phone_number == number:
                 return contact
+    @classmethod
+    def contact_exists(cls,number):
+        '''
+        Method that checks if a contact exists from the contact list.
+        Args:
+            number: Phone number to search if it exists
+        Returns :
+            Boolean: True or false depending if the contact exists
+        '''
+        for contact in cls.contact_list:
+            if contact.phone_number == number:
+                return True
+        return False
